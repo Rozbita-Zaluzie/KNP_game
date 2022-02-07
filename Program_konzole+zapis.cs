d37e2c03c8ef70e5dfd0eab3b70487b47ed14d8b
Rozbita-Zaluzie <64742692+Rozbita-Zaluzie@users.noreply.github.com>
2022-02-07 19:59:10 +0100

    Add files via upload

---
+++ Program_konzole+zapis.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+
+namespace _15___knp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                CreateFile();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Někde se stala chyba :(");
+                throw;
+            }
+
+            Console.Write("\ntoto je hra kámen, nůžky, papír.\nkolik chcete hrát kol ?\n počet kol - ");
+            bool okola = int.TryParse(Console.ReadLine(), out int kola);
+            Zapis("\ntoto je hra kámen, nůžky, papír.\nkolik chcete hrát kol ?\n počet kol - " + kola);
+            int skorehr = 0;
+            int skorepc = 0;
+            string zapis;
+
+            
+            knp();
+            void knp() //hra
+            {
+                for (int i = 0; i < kola; i++)
+                {
+                    Console.Write("\nmomentální skóre je [ " + skorehr + " | " + skorepc + " ]\nvyberte jednu z možností\n\n1 - kámen\n2 - nůžky\n3 - papír\n\n [ 1 | 2 | 3 ] - ");
+                    Zapis("\nmomentální skóre je [ " + skorehr + " | " + skorepc + " ]\nvyberte jednu z možností\n\n1 - kámen\n2 - nůžky\n3 - papír\n\n [ 1 | 2 | 3 ] - ");
+                    bool ok = int.TryParse(Console.ReadLine(), out int vyb);
+
+                    Random rand = new Random();
+                    int vybpc = rand.Next(1, 4);
+                    if (ok)
+                    {
+                        if (vyb > 0 && vyb < 4)
+                        {
+                            if (vybpc == 1)
+                            {
+                                if (vyb == 1)
+                                {
+                                    Console.WriteLine("\nhráč      - kámen\nprotihráč - kámen\n\n====================\nremíza !\n====================");
+                                    Zapis("\nhráč      - kámen\nprotihráč - kámen\n\n====================\nremíza !\n====================");
+
+                                }
+                                else if (vyb == 2)
+                                {
+                                    Console.WriteLine("\nhráč      - nůžky\nprotihráč - kámen\n\n====================\nprotihráč vyhrává !\n====================");
+                                    Zapis("\nhráč      - nůžky\nprotihráč - kámen\n\n====================\nprotihráč vyhrává !\n====================");
+                                    skorepc++;
+
+                                }
+                                else if (vyb == 3)
+                                {
+                                    Console.WriteLine("\nhráč      - papír\nprotihráč - kámen\n\n====================\nvyhrál jste !\n====================");
+                                    Zapis("\nhráč      - papír\nprotihráč - kámen\n\n====================\nvyhrál jste !\n====================");
+                                    skorehr++;
+
+                                }
+                            }
+                            else if (vybpc == 2)
+                            {
+                                if (vyb == 1)
+                                {
+                                    Console.WriteLine("\nhráč      - kámen\nprotihráč - nůžky\n\n====================\nvyhrál jste !\n====================");
+                                    Zapis("\nhráč      - kámen\nprotihráč - nůžky\n\n====================\nvyhrál jste !\n====================");
+                                    skorehr++;
+
+                                }
+                                else if (vyb == 2)
+                                {
+                                    Console.WriteLine("\nhráč      - nůžky\nprotihráč - nůžky\n\n====================\nremíza !\n====================");
+                                    Zapis("\nhráč      - nůžky\nprotihráč - nůžky\n\n====================\nremíza !\n====================");
+
+                                }
+                                else if (vyb == 3)
+                                {
+                                    Console.WriteLine("\nhráč      - papír\nprotihráč - nůžky\n\n====================\nprotihráč vyhrává !\n====================");
+                                    Zapis("\nhráč      - papír\nprotihráč - nůžky\n\n====================\nprotihráč vyhrává !\n====================");
+                                    skorepc++;
+
+                                }
+                            }
+                            else if (vybpc == 3)
+                            {
+                                if (vyb == 1)
+                                {
+                                    Console.WriteLine("\nhráč      - kámen\nprotihráč - papír\n\n====================\nprotihráč vyhrává !\n====================");
+                                    Zapis("\nhráč      - kámen\nprotihráč - papír\n\n====================\nprotihráč vyhrává !\n====================");
+                                    skorepc++;
+
+                                }
+                                else if (vyb == 2)
+                                {
+                                    Console.WriteLine("\nhráč      - nůžky\nprotihráč - papír\n\n====================\nvyhrál jste !\n====================");
+                                    Zapis("\nhráč      - nůžky\nprotihráč - papír\n\n====================\nvyhrál jste !\n====================");
+                                    skorehr++;
+
+                                }
+                                else if (vyb == 3)
+                                {
+                                    Console.WriteLine("\nhráč      - papír\nprotihráč - papír\n\n====================\nremíza !\n====================");
+                                    Zapis("\nhráč      - papír\nprotihráč - papír\n\n====================\nremíza !\n====================");
+
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nvyberte číslo mezi 1 - 3\nzkuste to znovu");
+                            Zapis("\nvyberte číslo mezi 1 - 3\nzkuste to znovu");
+                            knp();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nvyberte 1, 2, 3\nzkuste to znovu.");
+                        Zapis("\nvyberte 1, 2, 3\nzkuste to znovu.");
+                        knp();
+                    }
+                }
+                dalsihra();
+            }
+
+
+            void dalsihra() // hrát znovu ?
+            {
+                Console.Clear();
+                Console.WriteLine("\nkonečné skóre je\n\nhráč      - " + skorehr + "\nprotihráč - " + skorepc);
+                if (skorehr == skorepc)
+                {
+                    Console.WriteLine("\n====================\nRemíza !\n====================");
+                    Zapis("\n====================\nRemíza !\n====================");
+                }
+                else if (skorehr > skorepc)
+                {
+                    Console.WriteLine("\n====================\nVyhrál jste !\n====================");
+                    Zapis("\n====================\nVyhrál jste !\n====================");
+                }
+                else if (skorepc > skorehr)
+                {
+                    Console.WriteLine("\n====================\nVyhrává protihráč !\n====================");
+                    Zapis("\n====================\nVyhrává protihráč !\n====================");
+                }
+                Console.Write("\n\nchcete hrát znovu ?\n\n- ano\n- ne\n\n další hra ?  - ");
+                Zapis("\n\nchcete hrát znovu ?\n\n- ano\n- ne\n\n další hra ?  - ");
+                string dlsv = Console.ReadLine();
+                if (dlsv == "ano")
+                {
+                    Console.Clear();
+                    knp();
+                }
+                else { }
+            }
+
+            void CreateFile()
+            {
+                if (File.Exists("KNP_zaznam.txt"))
+                {
+                    zapis = "KNP_zaznam.txt";
+                    File.WriteAllText(zapis, "");
+                }
+                else
+                {
+                    StreamWriter writer = new StreamWriter("KNP_zaznam.txt");
+                    zapis = "KNP_zaznam.txt";
+                }
+            }
+
+            void Zapis(string text)
+            {
+                using (StreamWriter sw = File.AppendText(zapis))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+        }
+    }
+}
---
+++ Program_konzole.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace _15___knp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\ntoto je hra kámen, nůžky, papír.\n");
+            knp();
+            void knp() //hra
+            {
+                Console.Write("\nvyberte jednu z možností\n\n1 - kámen\n2 - nůžky\n3 - papír\n\n [ 1 | 2 | 3 ] - ");
+                bool ok = int.TryParse(Console.ReadLine(),out int vyb);
+
+                Random rand = new Random();
+                int vybpc = rand.Next(1,4);
+                if (ok)
+                {
+                    if (vyb > 0 && vyb < 4)
+                    {
+                        if (vybpc == 1)
+                        {
+                            if (vyb == 1)
+                            {
+                                Console.WriteLine("\nhráč      - kámen\nprotihráč - kámen\n\n====================\nremíza !\n====================");
+                                dalsihra();
+                            }
+                            else if (vyb == 2)
+                            {
+                                Console.WriteLine("\nhráč      - nůžky\nprotihráč - kámen\n\n====================\nprotihráč vyhrává !\n====================");
+                                dalsihra();
+                            }
+                            else if (vyb == 3)
+                            {
+                                Console.WriteLine("\nhráč      - papír\nprotihráč - kámen\n\n====================\nvyhrál jste !\n====================");
+                                dalsihra();
+                            }
+                        }
+                        else if (vybpc == 2)
+                        {
+                            if (vyb == 1)
+                            {
+                                Console.WriteLine("\nhráč      - kámen\nprotihráč - nůžky\n\n====================\nvyhrál jste !\n====================");
+                                dalsihra();
+                            }
+                            else if (vyb == 2)
+                            {
+                                Console.WriteLine("\nhráč      - nůžky\nprotihráč - nůžky\n\n====================\nremíza !\n====================");
+                                dalsihra();
+                            }
+                            else if (vyb == 3)
+                            {
+                                Console.WriteLine("\nhráč      - papír\nprotihráč - nůžky\n\n====================\nprotihráč vyhrává !\n====================");
+                                dalsihra();
+                            }
+                        }
+                        else if (vybpc == 3)
+                        {
+                            if (vyb == 1)
+                            {
+                                Console.WriteLine("\nhráč      - kámen\nprotihráč - papír\n\n====================\nprotihráč vyhrává !\n====================");
+                                dalsihra();
+                            }
+                            else if (vyb == 2)
+                            {
+                                Console.WriteLine("\nhráč      - nůžky\nprotihráč - papír\n\n====================\nvyhrál jste !\n====================");
+                                dalsihra();
+                            }
+                            else if (vyb == 3)
+                            {
+                                Console.WriteLine("\nhráč      - papír\nprotihráč - papír\n\n====================\nremíza !\n====================");
+                                dalsihra();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nvyberte číslo mezi 1 - 3\nzkuste to znovu");
+                        knp();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nvyberte 1, 2, 3\nzkuste to znovu.");
+                    knp();
+                }
+            }
+
+
+            void dalsihra() // hrát znovu ?
+            {
+                Console.WriteLine("\n\nchcete hrát znovu ?\n\n- ano\n- ne");
+                string dlsv = Console.ReadLine();
+                if (dlsv == "ano")
+                {
+                    Console.Clear();
+                    knp();
+                }
+                else { }
+            }
+        }
+    }
+}
